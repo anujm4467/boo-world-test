99bad0872b49975e394feaf2ab9a411983b5d6c3
Anuj Mishra <anujm4467@gmail.com>
2024-01-14 18:58:44 +0530

    fix: added mongo setup

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Get, Body, Query } from '@nestjs/common';
+import { Controller, Post, Get, Body, Query, Param } from '@nestjs/common';
 import {
   ApiTags,
   ApiOperation,
   ApiOkResponse,
   ApiBadRequestResponse,
+  ApiNotFoundResponse,
 } from '@nestjs/swagger';
 import { CommentService } from './comment.service';
 import { CommentDto, CommentQueryDto } from './dto';
@@ -45,5 +46,43 @@ export class CommentController {
     @Query() query: CommentQueryDto,
   ): Promise<IComment[]> {
     return this.commentService.getCommentsByProfileId(query);
+  }
+
+  /**
+   * Like a comment.
+   *
+   * @param {string} commentId - The ID of the comment to like.
+   * @param {string} profileId - The ID of the profile liking the comment.
+   * @returns {Promise<void>} A promise that resolves when the like operation is successful.
+   */
+  @Post(':commentId/like/:profileId')
+  @ApiOperation({ summary: 'Like a comment' })
+  @ApiOkResponse({ description: 'Comment successfully liked' })
+  @ApiNotFoundResponse({ description: 'Comment not found' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  async likeComment(
+    @Param('commentId') commentId: string,
+    @Param('profileId') profileId: string,
+  ): Promise<void> {
+    await this.commentService.likeComment(commentId, profileId);
+  }
+
+  /**
+   * Dislike a comment.
+   *
+   * @param {string} commentId - The ID of the comment to dislike.
+   * @param {string} profileId - The ID of the profile disliking the comment.
+   * @returns {Promise<void>} A promise that resolves when the dislike operation is successful.
+   */
+  @Post(':commentId/dislike/:profileId')
+  @ApiOperation({ summary: 'Dislike a comment' })
+  @ApiOkResponse({ description: 'Comment successfully disliked' })
+  @ApiNotFoundResponse({ description: 'Comment not found' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  async dislikeComment(
+    @Param('commentId') commentId: string,
+    @Param('profileId') profileId: string,
+  ): Promise<void> {
+    await this.commentService.dislikeComment(commentId, profileId);
   }
 }
